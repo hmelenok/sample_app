59bb53b2926a409b0531ff985da7d2a271b2c51a
hmelenok <hmelenok@hmelenok-R40-R41.(none)>
2013-09-20 13:09:13 +0300

    Add user microposts

--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -1,6 +1,4 @@
-=begin
 require 'spec_helper'
-
 
 describe Micropost do
 
@@ -28,5 +26,14 @@ describe Micropost do
     before { @micropost.user_id = nil }
     it { should_not be_valid }
   end
-end 
-=end
+
+  describe "with blank content" do
+    before { @micropost.content = " " }
+    it { should_not be_valid }
+  end
+
+  describe "with content that is too long" do
+    before { @micropost.content = "a" * 141 }
+    it { should_not be_valid }
+  end
+end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -115,7 +115,22 @@ describe "User pages" do
       it { should have_selector('title', text: "Edit user") }
       it { should have_link('change', href: 'http://gravatar.com/emails') }
     end
+    describe "profile page" do
+    let(:user) { FactoryGirl.create(:user) }
+    let!(:m1) { FactoryGirl.create(:micropost, user: user, content: "Foo") }
+    let!(:m2) { FactoryGirl.create(:micropost, user: user, content: "Bar") }
 
+    before { visit user_path(user) }
+
+    it { should have_selector('h1',    text: user.name) }
+    it { should have_selector('title', text: user.name) }
+
+    describe "microposts" do
+      it { should have_content(m1.content) }
+      it { should have_content(m2.content) }
+      it { should have_content(user.microposts.count) }
+      end
+    end
     describe "with invalid information" do
       before { click_button "Save changes" }
 
